7dddfc35772122c2c273c4580ab9a10418d41bf9
Joschka Braun <joschka.braun@gmail.com>
2020-04-06 21:07:55 -0400

    Finished task 3

--- hw3/omp-scan.cpp
+++ hw3/omp-scan.cpp
@@ -13,8 +13,48 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
   }
 }
 
-void scan_omp(long* prefix_sum, const long* A, long n) {
-  // TODO: implement multi-threaded OpenMP scan
+void scan_omp(long* prefix_sum, const long* A, long n, long p) {
+  if (n==0) return;
+  //int p = omp_get_max_threads();  		// num_threads
+  long* pre_part_sum = (long*) calloc(p, sizeof(long));
+  long* part_sum = (long*) calloc(p, sizeof(long));
+  //prefix_sum[0] = 0;
+  
+  #pragma omp parralel shared(p, prefix_part_sum, part_sum, np)
+  {
+  #pragma omp for
+  for (long i = 0; i < p; i++) {
+    long l = i*n/p;
+    prefix_sum[l] = 0;
+    for (long j = l+1; j < l+n/p; j++) {
+      prefix_sum[j] = prefix_sum[j-1] + A[j-1];
+    }
+    part_sum[i] = prefix_sum[l+n/p-1] + A[l+n/p-1];
+  }
+
+  #pragma omp single
+  {
+  scan_seq(pre_part_sum, part_sum, p);
+  }
+ 
+ // correcting error
+  #pragma omp for
+    for (long i = 1; i < p; i++) {
+      long l = i*n/p;
+      for (long j = l; j < l+n/p; j++) {
+        prefix_sum[j] = prefix_sum[j] + pre_part_sum[j];
+      }
+    }
+  }
+  
+  // handling the remaining data
+  for (long i = n/p*p; i < n; i++) {
+    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+  }
+
+
+  free(pre_part_sum);
+  free(part_sum);
 }
 
 int main() {
@@ -27,10 +67,13 @@ int main() {
   double tt = omp_get_wtime();
   scan_seq(B0, A, N);
   printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
-
-  tt = omp_get_wtime();
-  scan_omp(B1, A, N);
-  printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
+  
+  int thread_nums[] = {1,2,4};
+  for (int p : thread_nums) {
+    tt = omp_get_wtime();
+    scan_omp(B1, A, N, p);
+    printf("parallel-scan with %d threads = %fs\n", p, omp_get_wtime() - tt);
+  }
 
   long err = 0;
   for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
@@ -39,5 +82,6 @@ int main() {
   free(A);
   free(B0);
   free(B1);
+  delete thread_nums;
   return 0;
 }
