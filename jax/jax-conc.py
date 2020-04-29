4c025887ed16107c953051281be69299d2ffdcde
Joschka Braun <joschka.braun@gmail.com>
2020-04-29 19:38:23 -0400

    Serial and threading

--- jax/jax-conc.py
+++ jax/jax-conc.py
@@ -15,9 +15,14 @@ def f(x):
 def g(x):
     return x - 3.
 
+
 xs = [onp.random.randn(i) for i in range(10)]
+ed = jnp.ediff1d(xs[1], 0., -10.)
+
+print("now concurrency")
+
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    futures = [executor.submit(partial(g, x)) for x in xs]
+    futures = [executor.submit(partial(f, x)) for x in xs]
     ys = [f.result() for f in futures]
 for x, y in zip(xs, ys):
     if not all(jnp.equal(x,y)):
