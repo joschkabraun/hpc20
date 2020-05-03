2ce527dd90d5e73e2f254ee6d63c6eb43066b4ba
Joschka Braun <joschka.braun@gmail.com>
2020-05-02 22:02:20 -0400

    Testing on prince

--- hw5/runscript.sh
+++ hw5/runscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 #SBATCH --job-name=hpc20-hw5-jacobi2d
-#SBATCH --nodes=4
-#SBATCH --tasks-per-node=4
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=16
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=2GB
 #SBATCH --time=00:10:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jjb666@nyu.edu
@@ -13,4 +13,5 @@
 
 module purge
 module load openmpi/gnu/4.0.2
-./jacobi_mpi 2 10000 160
+
+mpiexec -np 16 ./jacobi_mpi 2 10000 16
